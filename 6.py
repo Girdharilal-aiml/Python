2d76ca11074efef2d740441fefb56cfcd8fc6dd1
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-18 19:56:10 +0500

    Update 6.py

--- 6.py
+++ 6.py
@@ -7,3 +7,9 @@ import tkinter as tk
 from tkinter import messagebox
 import time
 
+class CountdownTimer:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Countdown Timer")
+        self.root.geometry("450x500")
+        self.root.configure(bg='#0f0f0f')
