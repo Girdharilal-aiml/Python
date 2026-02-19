50c4a0beea1d3cfeed7d6f08c76151e6147c64dd
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-19 20:03:15 +0500

    Update 7.py

--- 7.py
+++ 7.py
@@ -3,3 +3,9 @@ Color Code Generator
 Generate and preview colors with their codes (HEX, RGB)
 """
 
+import tkinter as tk
+from tkinter import messagebox
+import random
+
+class ColorGenerator:
+    def __init__(self, root):
