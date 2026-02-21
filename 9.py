f3263cf80f08637cda6e14bd3625747ed1dc12e3
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-21 22:01:03 +0500

    Update 9.py

--- 9.py
+++ 9.py
@@ -8,3 +8,10 @@ from tkinter import messagebox
 import random
 import string
 
+class PasswordGenerator:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Password Generator")
+        self.root.geometry("500x650")
+        self.root.configure(bg='#1a1a2e')
+        self.root.resizable(False, False)
