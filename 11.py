f88a0a9dda57ca0af10b2ae22c6d751657733d12
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-23 21:35:19 +0500

    Create 11.py

---
+++ 11.py
@@ -0,0 +1,14 @@
+"""
+QR Code Generator
+Generate QR codes from text/URLs
+Requires: pip install qrcode pillow
+"""
+
+import tkinter as tk
+from tkinter import messagebox, filedialog
+from PIL import Image, ImageTk
+import qrcode
+import io
+
+class QRCodeGenerator:
+    def __init__(self, root):
