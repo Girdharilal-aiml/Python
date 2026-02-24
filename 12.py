c89181722fcfa9e0002f1ae3636dd0215fe73104
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-24 14:46:10 +0500

    Update 12.py

--- 12.py
+++ 12.py
@@ -3,3 +3,13 @@ Stopwatch with Lap Timer - Modern UI
 Beautiful redesign with better visuals
 """
 
+import tkinter as tk
+from tkinter import font as tkfont
+import time
+
+class Stopwatch:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Stopwatch")
+        self.root.geometry("500x700")
+        self.root.configure(bg='#0d1117')
