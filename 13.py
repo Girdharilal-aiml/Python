23a1ab8baad8469d88cce71c0c2c9ea5bd456be9
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-25 22:02:12 +0500

    Update 13.py

--- 13.py
+++ 13.py
@@ -8,3 +8,9 @@ from tkinter import font as tkfont
 import math
 
 class BMICalculator:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("BMI Calculator")
+        self.root.geometry("500x700")
+        self.root.configure(bg='#0d1117')
+        self.root.resizable(False, False)
