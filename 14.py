9047dddc6d717435cd7f1483273b31eef80b92a6
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-26 20:02:32 +0500

    Update 14.py

--- 14.py
+++ 14.py
@@ -3,3 +3,14 @@ Unit Converter - Modern UI
 Convert between different units (temperature, length, weight, volume)
 """
 
+import tkinter as tk
+from tkinter import ttk, font as tkfont
+
+class UnitConverter:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Unit Converter")
+        self.root.geometry("550x650")
+        self.root.configure(bg='#0d1117')
+        self.root.resizable(False, False)
+
