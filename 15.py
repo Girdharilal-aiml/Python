ddd747f025c12c432cd2cef96c1e65d0b6120422
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-02-27 22:07:35 +0500

    Update 15.py

--- 15.py
+++ 15.py
@@ -8,3 +8,9 @@ from tkinter import ttk, font as tkfont
 from datetime import datetime, date
 
 class AgeCalculator:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Age Calculator")
+        self.root.geometry("550x700")
+        self.root.configure(bg='#0d1117')
+        self.root.resizable(False, False)
