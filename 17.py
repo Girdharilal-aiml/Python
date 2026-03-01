647c87980f0cfcd85d80f3b7a5f0a163dc1ad19e
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-03-01 14:23:08 +0500

    Update 17.py

--- 17.py
+++ 17.py
@@ -69,3 +69,11 @@ class Game2048:
             fg='#58a6ff'
         ).pack(side=tk.LEFT, padx=(0, 10))
 
+        tk.Label(
+            left_header,
+            text="2048",
+            font=self.title_font,
+            bg='#161b22',
+            fg='#c9d1d9'
+        ).pack(side=tk.LEFT)
+
