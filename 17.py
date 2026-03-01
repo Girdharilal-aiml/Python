225ab5d3b96d4b45f90aacd5248645a159180c0b
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-03-01 14:22:36 +0500

    Update 17.py

--- 17.py
+++ 17.py
@@ -3,3 +3,15 @@
 Classic sliding tile puzzle game
 """
 
+import tkinter as tk
+from tkinter import font as tkfont
+import random
+
+class Game2048:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("2048 Game")
+        self.root.geometry("600x750")
+        self.root.configure(bg='#0d1117')
+        self.root.resizable(True, True)
+        self.root.minsize(400, 500)
