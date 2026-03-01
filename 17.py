2e1914c1357f634c71641176d77a5606d3af22da
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-03-01 14:22:49 +0500

    Update 17.py

--- 17.py
+++ 17.py
@@ -22,3 +22,20 @@ class Game2048:
         self.score = 0
         self.best_score = 0
         self.game_over = False
+
+        # Colors for tiles
+        self.tile_colors = {
+            0: '#1a1a2e',
+            2: '#eee4da',
+            4: '#ede0c8',
+            8: '#f2b179',
+            16: '#f59563',
+            32: '#f67c5f',
+            64: '#f65e3b',
+            128: '#edcf72',
+            256: '#edcc61',
+            512: '#edc850',
+            1024: '#edc53f',
+            2048: '#edc22e',
+        }
+
