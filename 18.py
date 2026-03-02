fe9091cb9385e0327b2bf3b53830c1b8c862357f
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-03-02 13:02:22 +0500

    Update 18.py

--- 18.py
+++ 18.py
@@ -10,3 +10,17 @@ from PIL import Image, ImageTk, ImageEnhance, ImageFilter, ImageOps
 import os
 
 class ImageEditor:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Image Editor")
+        self.root.geometry("1000x700")
+        self.root.configure(bg='#0d1117')
+        self.root.resizable(True, True)
+        self.root.minsize(800, 600)
+
+        # Image state
+        self.original_image = None
+        self.current_image = None
+        self.display_image = None
+        self.photo = None
+        self.filename = None
