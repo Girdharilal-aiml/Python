d7b87e7852561e00fb1166bf14496dd35dba09d6
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-03-02 13:02:18 +0500

    Update 18.py

--- 18.py
+++ 18.py
@@ -3,3 +3,10 @@ Image Editor - Modern UI (Responsive)
 Edit images: crop, rotate, filters, brightness, contrast
 Requires: pip install pillow
 """
+
+import tkinter as tk
+from tkinter import filedialog, messagebox, font as tkfont
+from PIL import Image, ImageTk, ImageEnhance, ImageFilter, ImageOps
+import os
+
+class ImageEditor:
