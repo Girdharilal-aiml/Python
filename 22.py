8dee22facb18655232cc59e2af905c9e89afd0c7
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-03-06 13:22:39 +0500

    Update 22.py

--- 22.py
+++ 22.py
@@ -2,3 +2,11 @@
 Note-Taking App - Simple & Perfect UI
 Clean, functional note-taking app
 """
+
+import tkinter as tk
+from tkinter import messagebox
+import json
+import os
+from datetime import datetime
+
+        
