453fab45eaa778ce9619f7d6fe9ab5a830fafbdb
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-03-08 11:59:31 +0500

    Update 24.py

--- 24.py
+++ 24.py
@@ -7,4 +7,13 @@ import tkinter as tk
 from tkinter import messagebox
 import random
 
+class Breakout:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Breakout")
+        self.root.geometry("600x700")
+        self.root.configure(bg='white')
+        self.root.resizable(True, True)
+        self.root.minsize(500, 600)
+
         
