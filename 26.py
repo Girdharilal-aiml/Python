961d82b8fac0c644cfe459b53f655d69bcd5aa2e
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-03-10 20:46:59 +0500

    Update 26.py

--- 26.py
+++ 26.py
@@ -2,3 +2,9 @@
 Drawing/Paint App - Simple & Perfect UI
 Draw with different tools and colors
 """
+
+import tkinter as tk
+from tkinter import colorchooser, filedialog, messagebox
+from PIL import Image, ImageDraw
+
+
