9dbf1ec06f11927d39f94e232ad452e8d8978422
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-03-11 13:31:26 +0500

    Create 27.py

---
+++ 27.py
@@ -0,0 +1,11 @@
+"""
+Flashcard App - Simple & Perfect UI
+Create decks, study, and track progress
+"""
+
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+import json
+import os
+import random
+
