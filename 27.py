0abd4130fefe4a11c14e20a64da5b70e6bd62a79
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-03-11 13:31:31 +0500

    Update 27.py

--- 27.py
+++ 27.py
@@ -9,3 +9,12 @@ import json
 import os
 import random
 
+class FlashcardApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Flashcards")
+        self.root.geometry("800x700")
+        self.root.configure(bg='white')
+        self.root.resizable(True, True)
+        self.root.minsize(600, 500)
+
