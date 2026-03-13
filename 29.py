22012eb42e79dacccd9215f8835597cf6f562c1c
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-03-13 11:21:23 +0500

    Create 29.py

---
+++ 29.py
@@ -0,0 +1,10 @@
+"""
+Markdown Editor - Simple & Perfect UI
+Write markdown with live preview
+Requires: pip install markdown
+"""
+
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import markdown
+import os
