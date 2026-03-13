38a45460df35d0c437d24b230ed684c421c04273
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-03-13 11:22:06 +0500

    Update 29.py

--- 29.py
+++ 29.py
@@ -8,3 +8,13 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 import markdown
 import os
+
+class MarkdownEditor:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Markdown Editor")
+        self.root.geometry("1000x700")
+        self.root.configure(bg='white')
+        self.root.resizable(True, True)
+        self.root.minsize(800, 500)
+
