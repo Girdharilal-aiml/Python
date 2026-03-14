5e61999201c83bed57db75d597d4f1d7cd718fbc
Girdhari Lal <girdharilal.aiml@gmail.com>
2026-03-14 15:30:26 +0500

    Create 30.py

---
+++ 30.py
@@ -0,0 +1,12 @@
+"""
+Calendar/Planner - Simple & Perfect UI
+Monthly calendar with event management
+"""
+
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+import calendar
+import json
+import os
+from datetime import datetime, timedelta
+
