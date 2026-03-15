274e48d3bf79c4a0c7004c4dc037a9beaed25881
Girdhari lal <k250005@nu.edu.pk>
2026-03-15 13:31:27 -0700

    Update 31.py

--- 31.py
+++ 31.py
@@ -7,3 +7,8 @@ import tkinter as tk
 from tkinter import messagebox
 import random
 
+class MemoryGame:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Memory Card Game")
+        self.root.geometry("700x800")
