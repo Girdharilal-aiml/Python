db0ecff50ee3573580439aaad07bdc8e49f4b8ce
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-17 10:59:44 +0500

    Create 34.py

---
+++ 34.py
@@ -0,0 +1,13 @@
+"""
+File Search Tool - Simple & Perfect UI
+Search files with filters and preview
+"""
+
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import os
+from pathlib import Path
+import subprocess
+import sys
+
+class FileSearchTool:
