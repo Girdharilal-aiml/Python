69700f8251eda894b33677f7c3012fecd1415800
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-19 15:11:15 +0500

    Update 36.py

--- 36.py
+++ 36.py
@@ -8,3 +8,12 @@ from tkinter import messagebox, filedialog
 import json
 import os
 
+class ContactManager:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Contact Manager")
+        self.root.geometry("900x700")
+        self.root.configure(bg='white')
+        self.root.resizable(True, True)
+        self.root.minsize(800, 600)
+
