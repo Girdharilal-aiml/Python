3dc19b1ac0d786bd3c7f4abe734c3d93aacce069
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-19 15:11:07 +0500

    Create 36.py

---
+++ 36.py
@@ -0,0 +1,10 @@
+"""
+Contact Manager - Simple & Perfect UI
+Manage contacts with search and vCard export
+"""
+
+import tkinter as tk
+from tkinter import messagebox, filedialog
+import json
+import os
+
