9ba4194688e785ff05469e9f8e04a00e73fb3372
Girdhari lal <girdharilal.aiml@gmail.com>
2026-04-07 19:33:41 +0500

    Update 39.py

--- 39.py
+++ 39.py
@@ -3,3 +3,9 @@ eBook Reader - Simple & Perfect UI
 Read TXT/PDF files with bookmarks and table of contents
 Requires: pip install PyPDF2
 """
+
+import tkinter as tk
+from tkinter import filedialog, messagebox, scrolledtext
+import json
+import os
+
